518a19753190e6a46117c3413ac14b39d860f2db
Thomas Weinert <thomas@weinert.info>
2017-03-18 14:10:23 +0100

    defaults and positions, cleanup

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # reveal-badges
 Badges for revael.js
+
+After a talk, one of the attendees said that 
+it was sometimes a little difficult to put
+source examples into the right context. He suggested
+to add the language to the slides.
+
+That seems to be a good idea, so I wrote a plugin that
+add a badge to code blocks.
+
+If you use bower for your reveal.js presentations, you 
+can install the plugin using it.
+
+```
+bower install --save reveal-badges
+```
+
+After that add it to the dependencies section in your HTML file.
+
+```
+dependencies: [
+  { src: 'bower_components/reveal-badges/src/badges.js' },
+  ... 
+```
+
+## Options
+
+```
+badges: {
+  path: '/path/to/plugin',
+  defaults: {
+    bg: 'black',
+    fg: 'white',
+    position: 'tr'
+  },
+  languages: false
+}
+```
+
+### Path
+
+`@var {string} path`
+
+The plugin will try to recognize its installation path
+automatically. If that doe not work correctly, you can
+provide the path, that will be used to include additional
+plugin files.
+
+### Defaults
+
+`@var {Object} defaults`
+
+You can change the default colors and position for badges
+using the `defaults` option.
+
+* `bg` - background color
+* `fg` - text color
+* `position` - badge position (tr, br, bl, tl)
+
+### Languages
+
+`@var {boolean|{}} languages`
+
+If set to `true`, the plugin will add `data-badge` attributes to all
+`pre > code` constructs that have a `language-*` class. 
+It will use the language from the class name as text for the badge.
+
+You can use this property to configure the badge for a specific 
+language, too. 
+
+```
+{
+  languages: {
+    'php': {
+      label: 'PHP 7'
+      fg: 'white',
+      bg: 'black',
+      position: 'tr'
+    }
+  }
+}
+```
+
+#### Disable
+
+To disable the badge for a specific language, set it to `false`.
+
+```json
+{
+  languages: {
+    'plaintext' : false
+  }
+}
+```
+
+## Data Attributes
+
+You can add `data-badge` attributes to any element. A manually added
+data attribute will take priority. It allows you to override
+the defaults for a language badge or add individual badges.
+
+```html
+<pre data-badge="PHP 7.1" data-badge-fg="red">
+  <code class="language-php">...</code>
+</pre>
+```
+  
+  
--- src/badges.js
+++ src/badges.js
@@ -1,100 +1,167 @@
-var RevealBadges = window.RevealBadges || (
-  function() {
-    var config = Reveal.getConfig();
-    var options = config.badges || {};
-    options.path = options.path || scriptPath() || 'plugin/language-badges';
-    var languageColors = {
-      'make': ['black', 'white'],
-      'php': ['#4f5b93', 'black'],
-      'c': 'red'
-    }
-    if (options.languages instanceof Object) {
-      for (var language in options.languages) {
-        if (!options.languages.hasOwnProperty(language)) {
-          languageColors[language] = color;
-        }
-      }
-    }
-
-    var resource = document.createElement("link");
-    resource.rel = "stylesheet";
-    resource.href = options.path + "/badges.css";
-    document.querySelector("head").appendChild(resource);
-
-    setUp(options, languageColors);
-
-    function setUp(options, languageColors) {
-      var i, badge, badgeStyle;
-      if (options.languages) {
-        var blocks = document.querySelectorAll('pre > code');
-        for (i = 0; i < blocks.length; i++) {
-          publishLanguage(blocks[i], languageColors);
-        }
-      }
-      var badgeParents = document.querySelectorAll('[data-badge]');
-      for (i = 0; i < badgeParents.length; i++) {
-        badge = badgeParents[i].appendChild(
-          document.createElement('span')
-        )
-        badge.setAttribute('class', 'badge');
-        badge.appendChild(
-          document.createTextNode(
-            badgeParents[i].getAttribute('data-badge')
-          )
-        );
-        badgeStyle = '';
-        color = badgeParents[i].getAttribute('data-badge-fg');
-        if (color != '') {
-          badgeStyle += 'color: ' + color + ';';
-        }
-        color = badgeParents[i].getAttribute('data-badge-bg');
-        if (color != '') {
-          badgeStyle += 'background-color: ' + color + ';';
-        }
-        if (badgeStyle != '') {
-          badge.setAttribute('style', badgeStyle);
-        }
-      }
-    }
-
-    function publishLanguage(code, languageColors) {
-      var pre = code.parentNode;
-      var match, language, languageColors;
-      if (match = code.getAttribute('class').match(/\blanguage-(\S+)/)) {
-        language = match[1];
-        if (!pre.getAttribute('data-badge')) {
-          pre.setAttribute('data-badge', language)
-        }
-        if (!pre.getAttribute('data-badge-bg')) {
-          if (languageColor = languageColors[language.toLowerCase()]) {
-            if (languageColor instanceof Array) {
-              pre.setAttribute('data-badge-bg', languageColor[0]);
-              pre.setAttribute('data-badge-fg', languageColor[1]);
-            } else if (languageColor instanceof Object) {
-              pre.setAttribute('data-badge-bg', languageColor.bg);
-              pre.setAttribute('data-badge-fg', languageColor.fg);
-            } else {
-              pre.setAttribute('data-badge-bg', languageColor);
-            }
-          }
-        }
-      }
-    }
-
-    function scriptPath() {
-      // obtain plugin path from the script element
-      var path;
-      var end = -("/badges.js".length);
-      if (document.currentScript) {
-        path = document.currentScript.src.slice(0, end);
-      } else {
-        var scriptTag = document.querySelector('script[src$="/badges.js"]');
-        if (scriptTag) {
-          path = sel.src.slice(0, end);
-        }
-      }
-      return path;
+var RevealBadges = window.RevealBadges || (function() {
+  var languages = {
+    'makefile': {
+      bg: 'black', fg: 'white'
+    },
+    'php': {
+      bg: '#4f5b93', fg: 'black'
+    },
+    'c': {
+      bg: 'red', fg: 'white'
     }
   }
-)();
 
+  var setUp = function(options, languages) {
+    var positions = {
+      'tl': 'topLeft',
+      'tr': 'topRight',
+      'bl': 'bottomLeft',
+      'br': 'bottomRight',
+    }
+    var properties = {
+      'bg': '',
+      'fg': '',
+      'position': '',
+      'class': ''
+    };
+    var i, key, value;
+    var badge, container, badgeClass, badgeStyle;
+
+    if (options.languages) {
+      var blocks = document.querySelectorAll('pre > code');
+      for (i = 0; i < blocks.length; i++) {
+        addLanguageBadge(blocks[i], languages);
+      }
+    }
+    var badgeParents = document.querySelectorAll('[data-badge]');
+    for (i = 0; i < badgeParents.length; i++) {
+      container = badgeParents[i];
+      for (key in properties) {
+        value = container.getAttribute('data-badge-' + key);
+        if (value) {
+          properties[key] = value;
+        } else if (options.defaults[key]) {
+          properties[key] = options.defaults[key];
+        } else {
+          properties[key] = '';
+        }
+      }
+      badge = container.appendChild(
+          document.createElement('span')
+      )
+      badge.appendChild(
+          document.createTextNode(
+              badgeParents[i].getAttribute('data-badge')
+          )
+      );
+      badgeClass = 'badge';
+      if (properties.class) {
+        badgeClass += ' ' + properties.class;
+      }
+      if (positions[properties.position]) {
+        badgeClass += ' ' + positions[properties.position];
+      }
+      badgeStyle = '';
+      if (properties.fg != '') {
+        badgeStyle += 'color: ' + properties.fg + ';';
+      }
+      if (properties.bg != '') {
+        badgeStyle += 'background-color: ' + properties.bg + ';';
+      }
+
+      badge.setAttribute('class', badgeClass);
+      if (badgeStyle != '') {
+        badge.setAttribute('style', badgeStyle);
+      }
+    }
+  }
+
+  var addLanguageBadge = function(code, languages) {
+    var properties = ['bg', 'fg', 'position', 'class'];
+    var container = code.parentNode;
+    var match, language, settings, i, attributeName, currentValue;
+    if (match = code.getAttribute('class').match(/\blanguage-(\S+)/)) {
+      language = match[1].toLowerCase();
+      if (false === languages[language]) {
+        return;
+      }
+      if (settings = languages[language]) {
+        if (!container.getAttribute('data-badge')) {
+          container.setAttribute(
+              'data-badge',
+              settings.label || match[1].toUpperCase()
+          );
+        }
+        for (i = 0; i < properties.length; i++) {
+          attributeName = 'data-badge-' + properties[i];
+          if (
+              settings[properties[i]] && !container.getAttribute(attributeName)
+          ) {
+            container.setAttribute(
+                attributeName, settings[properties[i]]
+            );
+          }
+        }
+      } else {
+        if (!container.getAttribute('data-badge')) {
+          container.setAttribute('data-badge', match[1]);
+        }
+      }
+    }
+  }
+
+  var scriptPath = function() {
+    // obtain plugin path from the script element
+    var path;
+    var end = -("/badges.js".length);
+    if (document.currentScript) {
+      path = document.currentScript.src.slice(0, end);
+    } else {
+      var scriptTag = document.querySelector('script[src$="/badges.js"]');
+      if (scriptTag) {
+        path = sel.src.slice(0, end);
+      }
+    }
+    return path;
+  }
+
+  var merge = function() {
+    var result = {}, i, key, source;
+    for (i = 0; i < arguments.length; i++) {
+      source = arguments[i];
+      for (key in source) {
+        if (!source.hasOwnProperty(key)) {
+          continue;
+        }
+        try {
+          if (source[key].constructor == Object) {
+            result[key] = merge(result[key], source[key]);
+          } else {
+            result[key] = source[key];
+          }
+        } catch (e) {
+          result[key] = source[key];
+        }
+      }
+    }
+    return result;
+  }
+
+  var config = Reveal.getConfig() || {};
+  config.badges = config.badges || {};
+  var options = {
+    path: config.badges.path || scriptPath() || 'plugin/language-badges',
+    languages: config.badges.languages,
+    defaults: config.badges.defaults || {}
+  }
+  if (options.languages instanceof Object) {
+    languages = merge(languages, options.languages);
+  }
+
+  var resource = document.createElement("link");
+  resource.rel = "stylesheet";
+  resource.href = options.path + "/badges.css";
+  document.querySelector("head").appendChild(resource);
+
+  setUp(options, languages);
+})();
